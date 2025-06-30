0da6aefe035c229cef9d459bd7410e092664e7ae
HosseinOutward <hossenmsignin@gmail.com>
2025-06-30 15:07:51 +0330

    removed the global var definitions and replaced it with class

---
+++ components/broadcast_components/broadcasting_process/WZ_broadcast.py
@@ -0,0 +1,86 @@
+from typing import List, Dict
+import numpy as np
+import torch
+
+from components.broadcast_components.compressor.entropy_coding import entropy_coding, entropy_decoding
+from components.broadcast_components.quantizer.simple import simple_quantize, simple_dequantize
+from components.broadcast_components.quantizer.wz_quant_ANN import WZQuantizer
+from components.broadcast_components.reporting_utilities import ReportingUtilities
+
+# ------------------
+# todo: dont use in import file definition
+wz_quantizer = WZQuantizer()
+reporting_util = ReportingUtilities()
+# ------------------
+
+
+def dict_to_array_and_normalize(grad_dict: Dict, min_v: List, max_v: List):
+    res = []
+    for i, (k, v) in enumerate(grad_dict.items()):
+        v = v.ravel() * 1000
+        v = (v - min_v[i] * 1000) / (max_v[i] * 1000 - min_v[i] * 1000)
+        v = v*2-1  # normalize to [-1, 1]
+        res.append(v.to('cpu').numpy())
+    res = np.concatenate(res)
+    return res
+
+
+def recover_shape_and_denormal_to_dict(grad_vector, org_shapes_dict, min_v: List, max_v: List):
+    res = {}
+    start = 0
+    for i, (k, shape) in enumerate(org_shapes_dict.items()):
+        end = start + np.prod(shape)
+        v = grad_vector[start:end]
+        v = (v+1)/2
+        v = v * (max_v[i] - min_v[i]) + min_v[i]
+        res[k] = v.reshape(shape)
+        start = end
+    return res
+
+
+@reporting_util.record_compr_stats
+def wz_encoding_process(worker_grad_dict, agent_id):
+    min_v, max_v = [
+        [f(v).to('cpu').numpy() for k, v in worker_grad_dict.items()]
+        for f in [torch.min, torch.max]]
+
+    # worker_grad_dict={k:v*1.1 for k, v in worker_grad_dict.items()}
+    # return worker_grad_dict, min_v, max_v, 0
+
+    grad_flat_normal = dict_to_array_and_normalize(worker_grad_dict, min_v, max_v)
+
+    quantizer = simple_quantize if agent_id <= 1 else wz_quantizer.encode
+    quantized_data = quantizer(grad_flat_normal)
+
+    dtype = quantized_data.dtype
+    encoded_data = entropy_coding(quantized_data)
+
+    return encoded_data, min_v, max_v, dtype
+
+
+@reporting_util.report_compression_wrapper
+def wz_reconstruction_process(worker_broadcast_data, agent_id, worker_count, global_model_dims, previous_data, ):
+    # return worker_broadcast_data[0]
+    encoded_data, min_v, max_v, dtype = worker_broadcast_data
+
+    quantized_decoded_data = entropy_decoding(encoded_data, dtype)
+
+    if agent_id <= 1:
+        res_vector = simple_dequantize(quantized_decoded_data, np.float32)
+        if agent_id == 1:
+            assert len(previous_data) == 1
+            temp = [
+                [f(v).to('cpu').numpy() for k, v in previous_data[0].items()]
+                for f in [torch.min, torch.max]]
+            prev_data = dict_to_array_and_normalize(previous_data[0], *temp)
+            wz_quantizer.train_model(res_vector, prev_data, )
+    else:
+        res_vector = wz_quantizer.decode(quantized_decoded_data, previous_data)
+
+    result_dict = recover_shape_and_denormal_to_dict(
+        res_vector, global_model_dims, min_v, max_v)
+
+    result_dict = {k: torch.tensor(v).to('cuda') for k, v in result_dict.items()}
+
+    return result_dict
+
--- components/broadcast_components/reporting_utilities.py
+++ components/broadcast_components/reporting_utilities.py
@@ -5,137 +5,143 @@ from components.broadcast_components.compressor.entropy_coding import entropy_co
 from components.broadcast_components.quantizer.simple import simple_quantize
 from components.other_utilities.models_to_train import ResNetPLModel
 
-# ------------------
-r_keys_state = [k for k, v in ResNetPLModel(
-    num_classes=10, resnet_version='resnet18').named_parameters() if v.requires_grad]
 
-r_original_dict_list = {k: [] for k in r_keys_state}
-r_byte_size_dict_list = {tt: [] for tt in ['raw', 'wz', 'entropy']}
-r_decomp_error_dict_list = {k: [] for k in r_keys_state}
-r_total_error = []
+class ReportingUtilities:
+    def __init__(self, ):
+        model_instance = ResNetPLModel(num_classes=10, resnet_version='resnet18')
 
-state_report_per_round = []
+        self.param_keys = [k for k, v in model_instance.named_parameters() if v.requires_grad]
 
+        self.org_grad_dict_list = {k: [] for k in self.param_keys}
+        self.recons_err_perc_dict_list = {k: [] for k in self.param_keys}
+        self.byte_mb_size_per_method = {tt: [] for tt in ['raw', 'wz', 'entropy']}
+        self.total_error = []
 
-# ------------------
+        self.state_report_per_round = []
 
+    def record_compr_stats(self, func):
+        from components.broadcast_components.broadcasting_process.WZ_broadcast import dict_to_array_and_normalize
 
-def report_compression_stat(func):
-    from components.broadcast_components.WZ_broadcast import dict_to_array_and_normalize
+        def wrapper(worker_grad_dict, agent_id):
+            assert agent_id == len(self.org_grad_dict_list[self.param_keys[0]]), \
+                "something wrong with the order of execution, agent_id given too soon"
 
-    def wrapper(worker_grad_dict, agent_id):
-        global r_original_dict_list, r_byte_size_dict_list, \
-            r_decomp_error_dict_list, r_total_error, r_keys_state
+            # record org grads sent ---------------
+            for k in self.param_keys:
+                self.org_grad_dict_list[k].append(worker_grad_dict[k])
 
-        assert agent_id == len(r_original_dict_list[r_keys_state[0]]), \
-            "something wrong with the order of execution, agent_id given too soon"
+            org_data_byte_size = sum(v.nbytes for v in worker_grad_dict.values()) / 1024 ** 2
 
-        r_original_dict_list = {k: v + [worker_grad_dict[k]]
-                                for k, v in r_original_dict_list.items()}
+            # Call the original function to get wz encoded data ---------------
+            encoded_data, min_v, max_v, dtype = func(worker_grad_dict, agent_id)
 
-        # Call the original function to get encoded data ---------------
-        encoded_data, min_v, max_v, dtype = func(worker_grad_dict, agent_id)
+            min_max_vec_size=2 * min_v[0].dtype.itemsize * len(min_v) / 1024 ** 2
+            enc_data_byte_size = (len(encoded_data) / 1024 ** 2 + min_max_vec_size)
 
-        # simulate entropy only coding ------------------------------
-        entr_grad_flat_normal = dict_to_array_and_normalize(worker_grad_dict, min_v, max_v)
-        entr_quantized_data = simple_quantize(entr_grad_flat_normal)
-        entr_encoded_data = entropy_coding(entr_quantized_data)
+            # simulate entropy only coding ------------------------------
+            entr_grad_flat_normal = dict_to_array_and_normalize(worker_grad_dict, min_v, max_v)
+            entr_quantized_data = simple_quantize(entr_grad_flat_normal)
+            entr_encoded_data = entropy_coding(entr_quantized_data)
 
-        enc_data_byte_size = len(encoded_data) / 1024**2
-        org_data_byte_size = sum(v.nbytes for v in worker_grad_dict.values()) / 1024**2
-        entr_enc_data_byte_size = len(entr_encoded_data)/1024**2
+            entr_enc_data_byte_size = len(entr_encoded_data) / 1024 ** 2
 
-        r_byte_size_dict_list['raw'].append(org_data_byte_size)
-        r_byte_size_dict_list['wz'].append(enc_data_byte_size)
-        r_byte_size_dict_list['entropy'].append(entr_enc_data_byte_size)
+            # recoding ------------------------------
+            self.byte_mb_size_per_method['raw'].append(org_data_byte_size)
+            self.byte_mb_size_per_method['wz'].append(enc_data_byte_size)
+            self.byte_mb_size_per_method['entropy'].append(entr_enc_data_byte_size)
 
-        return encoded_data, min_v, max_v, dtype
+            return encoded_data, min_v, max_v, dtype
 
-    return wrapper
+        return wrapper
 
+    def report_compression_wrapper(self, func):
+        def wrapper(worker_broadcast_data, agent_id, worker_count, global_model_dims, previous_data, ):
+            assert agent_id == len(self.recons_err_perc_dict_list[self.param_keys[0]]), \
+                "something wrong with the order of execution, agent_id given too soon"
 
-def report_decompression_stat(func):
-    def wrapper(worker_broadcast_data, agent_id, worker_count, global_model_dims, previous_data, ):
-        global r_original_dict_list, r_byte_size_dict_list, \
-            r_decomp_error_dict_list, r_total_error, r_keys_state
+            result_dict = func(worker_broadcast_data, agent_id, worker_count, global_model_dims, previous_data, )
 
-        assert agent_id == len(r_decomp_error_dict_list[r_keys_state[0]]), \
-            "something wrong with the order of execution, agent_id given too soon"
+            error_dict = {}
+            total_error = 0
+            total_element_count = sum(v[0].numel() for v in self.org_grad_dict_list.values())
+            for k, v in result_dict.items():
+                v = torch.Tensor(v).cuda()
+                temp = self.org_grad_dict_list[k][agent_id]
+                error_dict[k] = (v - temp).abs().sum()
+                error_dict[k] = (error_dict[k] / temp.abs().sum()).item()
+                total_error += error_dict[k] * (temp.numel() / total_element_count)
 
-        result_dict = func(worker_broadcast_data, agent_id, worker_count, global_model_dims, previous_data, )
+            self.recons_err_perc_dict_list = {k: v + [error_dict[k]]
+                                              for k, v in self.recons_err_perc_dict_list.items()}
+            self.total_error.append(total_error)
 
-        error_dict = {}
-        total_error = 0
-        total_element_count = sum(v[0].numel() for v in r_original_dict_list.values())
-        for k, v in result_dict.items():
-            v = torch.Tensor(v).cuda()
-            temp = r_original_dict_list[k][agent_id]
-            error_dict[k] = (v - temp).abs().sum()
-            error_dict[k] = (error_dict[k] / temp.abs().sum()).item()
-            total_error += error_dict[k] * (temp.numel() / total_element_count)
+            # Collecting decompression error statistics and reset ---------------
+            temp = agent_id == worker_count - 1
+            if self.total_error is None or temp:
+                if temp:
+                    self.state_report_per_round.append({
+                        'MB size info per agent': self.byte_mb_size_per_method,
+                        '% error per layer per agent': self.recons_err_perc_dict_list,
+                        '% total error per agent': self.total_error,
+                    })
+                self.org_grad_dict_list = {k: [] for k in self.param_keys}
+                self.byte_mb_size_per_method = {tt: [] for tt in ['raw', 'wz', 'entropy']}
+                self.recons_err_perc_dict_list = {k: [] for k in self.param_keys}
+                self.total_error = []
 
-        r_decomp_error_dict_list = {k: v + [error_dict[k]]
-                                    for k, v in r_decomp_error_dict_list.items()}
-        r_total_error.append(total_error)
+            return result_dict
 
-        # Collecting decompression error statistics and reset ---------------
-        temp = agent_id == worker_count - 1
-        if r_total_error is None or temp:
-            if temp:
-                state_report_per_round.append({
-                    'KB size info per agent': r_byte_size_dict_list,
-                    '% error per layer per agent': r_decomp_error_dict_list,
-                    '% total error per agent': r_total_error,
-                })
-            r_original_dict_list = {k: [] for k in r_keys_state}
-            r_byte_size_dict_list = {tt: [] for tt in ['raw', 'wz', 'entropy']}
-            r_decomp_error_dict_list = {k: [] for k in r_keys_state}
-            r_total_error = []
-
-        return result_dict
-
-    return wrapper
+        return wrapper
 
 
 if __name__ == "__main__":
-    from components.broadcast_components.WZ_broadcast import (
-            wz_reconstruction_process, wz_quantizer, wz_encoding_process)
+    from components.broadcast_components.broadcasting_process.WZ_broadcast import (
+        wz_reconstruction_process, wz_quantizer, wz_encoding_process, reporting_util)
     from experiments.resnet_parameter_corr_between_worker import load_grad_files
 
-    model_shape_dict = {
-        k: v.shape for k, v in ResNetPLModel(
-            num_classes=10, resnet_version='resnet18'
-        ).named_parameters() if v.requires_grad
-    }
+    torch.set_float32_matmul_precision('high')
+    import logging
+    logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
+    import warnings
+    warnings.filterwarnings("ignore", message="Starting from v1.9.0, `tensorboardX` has been removed")
+    warnings.filterwarnings("ignore", message="You defined a `validation_step` but have no `val_dataloader`")
+    warnings.filterwarnings("ignore", message="Consider setting `persistent_workers=True` in 'train_dataloader'")
 
     wz_quantizer.metric_report_flag = True
 
-    temp = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    w_i_grad_dict = load_grad_files(
-        temp, r_keys_state,
-        [f"../experiments/exp_data/gradients_resnet"
-         f"/adam/gradients_resnet_t{i}/" for i in range(2)],
-        curr_round=0, current_epoch=0)
-    w_i_grad_dict = [
-        {k: torch.tensor(v[i][j]).to('cuda') for k, v in w_i_grad_dict.items()}
-        for j in range(2) for i in range(len(list(w_i_grad_dict.values())[0]))]
-    w_i_grad_dict = [
-        {k: v.reshape(model_shape_dict[k]) for k, v in w_i_grad_dict[0].items()}
-        for w in w_i_grad_dict]
-    w_i_grad_dict = [w_i_grad_dict[:4], w_i_grad_dict[4:]]
+    model_shape_dict = {
+        k: v.shape
+        for k, v in ResNetPLModel(num_classes=10, resnet_version='resnet18').named_parameters() if v.requires_grad
+    }
 
-    for ww in w_i_grad_dict:
+    # load testing data
+    temp = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    grad_test_data = load_grad_files(
+        temp, list(model_shape_dict.keys()),
+        [f"../../experiments/exp_data/gradients_resnet/adam/gradients_resnet_t{2}/" for i in range(2)],
+        curr_round=0, current_epoch=0
+    )
+    grad_test_data = [
+        {k: torch.tensor(v[i][j]).reshape(model_shape_dict[k]).to('cuda')
+            for k, v in grad_test_data.items()}
+        for j in range(2) for i in range(len(list(grad_test_data.values())[0]))
+    ]
+    grad_test_data = [grad_test_data[:len(temp)], grad_test_data[len(temp):]]
+
+    # simulate the WZ encoding and reconstruction process
+    for ww in grad_test_data:
         prev = []
         for ag_id in range(len(ww)):
             broadcast_data = wz_encoding_process(ww[ag_id], ag_id)
             decoded_agent_broadcast = wz_reconstruction_process(
-                ag_id, len(ww), model_shape_dict, prev, broadcast_data)
-            prev += [decoded_agent_broadcast]
+                broadcast_data, ag_id, len(ww), model_shape_dict, prev, )
+            prev.append(decoded_agent_broadcast)
 
+    # report
     print("Compression Reporting:")
-    for i, report in enumerate(state_report_per_round):
+    for i, report in enumerate(reporting_util.state_report_per_round):
         print(f"Round {i}:")
-        print("Byte Size Info per Agent:", report['byte size info per agent'])
-        print("% Error per Layer per Agent:", report['% error per layer per agent'])
+        print("Byte Size Info per Agent:", report['MB size info per agent'])
+        # print("% Error per Layer per Agent:", report['% error per layer per agent'])
         print("% Total Error per Agent:", report['% total error per agent'])
         print()
