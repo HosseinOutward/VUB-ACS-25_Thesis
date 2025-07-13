b446413a8270307332d1404306d8d09b4a927379
HosseinOutward <hossenmsignin@gmail.com>
2025-07-13 13:46:00 +0330

    added testing code for wz process (without reporting parts)

--- components/broadcast_components/broadcasting_process/WZ_broadcast.py
+++ components/broadcast_components/broadcasting_process/WZ_broadcast.py
@@ -1,13 +1,27 @@
 from typing import List, Dict
 import numpy as np
 import torch
+from lightning import seed_everything
 
 from components.broadcast_components.compressor.entropy_coding import entropy_coding, entropy_decoding
 from components.broadcast_components.quantizer.wz_quant_ANN import WZQuantizer, PL_EncoderDecoder_ANN
-from components.broadcast_components.quantizer.wz_quant_RNN import WZQuantizerRNN, PL_EncoderDecoder_RNN
+from components.broadcast_components.quantizer.wz_quant_RNN import PL_EncoderDecoder_RNN
 
 
-def dict_to_array_and_normalize(grad_dict: Dict, min_v: List, max_v: List):
+def dict_to_array_and_normalize(grad_dict: Dict):
+    # min_v, max_v = [
+    #     [f(v).to('cpu').numpy() for k, v in grad_dict.items()] for f in [torch.min, torch.max]]
+
+    clip_range_min, clip_range_max = [
+        [f(v.to('cpu').numpy()) for k, v in grad_dict.items()]
+        for f in [lambda x: np.percentile(x,0.002), lambda x: np.percentile(x,99.998)] ]
+    grad_dict = {k: torch.clip(v, clip_range_min[i], clip_range_max[i])
+                 for i, (k, v) in enumerate(grad_dict.items())}
+
+    min_v, max_v = [
+        [f(v.to('cpu').numpy()) for k, v in grad_dict.items()]
+        for f in [lambda x: np.percentile(x,0.01), lambda x: np.percentile(x,99.99)] ]
+
     res = []
     for i, (k, v) in enumerate(grad_dict.items()):
         v = v.ravel() * 1000
@@ -15,7 +29,7 @@ def dict_to_array_and_normalize(grad_dict: Dict, min_v: List, max_v: List):
         v = v * 2 - 1  # normalize to [-1, 1]
         res.append(v.to('cpu').numpy())
     res = np.concatenate(res)
-    return res
+    return res, min_v, max_v
 
 
 def recover_shape_and_denormal_to_dict(grad_vector, org_shapes_dict, min_v: List, max_v: List):
@@ -47,17 +61,10 @@ class WZBroadcastProtocol:
 
     # %%
     def wz_encoding_process(self, worker_grad_dict, agent_id):
-        min_v, max_v = [
-            [f(v).to('cpu').numpy() for k, v in worker_grad_dict.items()] for f in [torch.min, torch.max]]
-
-        # min_v, max_v = [
-        #     [f(v.to('cpu').numpy()) for k, v in worker_grad_dict.items()]
-        #     for f in [lambda x: np.percentile(x,0.01), lambda x: np.percentile(x,99.99)] ]
-
         # worker_grad_dict={k:v*1.1 for k, v in worker_grad_dict.items()}
         # return worker_grad_dict, min_v, max_v, 0
 
-        grad_flat_normal = dict_to_array_and_normalize(worker_grad_dict, min_v, max_v)
+        grad_flat_normal, min_v, max_v = dict_to_array_and_normalize(worker_grad_dict)
 
         quantizer = self.wz_quantizer_list[agent_id]
         quantized_data = quantizer.encoding_process(grad_flat_normal)
@@ -120,3 +127,47 @@ class WZBroadcastProtocol:
         )
         self.wz_quantizer_list[next_agent].train_model(
             last_recent_grads, self.side_info_data_list, batch_size=10_000)
+
+
+if __name__ == "__main__":
+    # --------------------------------
+    torch.set_float32_matmul_precision('medium')
+    import logging
+    logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
+    import warnings
+    warnings.filterwarnings("ignore", message="Starting from v1.9.0, `tensorboardX` has been removed")
+    warnings.filterwarnings("ignore", message="You defined a `validation_step` but have no `val_dataloader`")
+    warnings.filterwarnings("ignore", message="Consider setting `persistent_workers=True` in 'train_dataloader'")
+    warnings.filterwarnings("ignore", message="The 'val_dataloader' does not have")
+
+    worker_count = 4
+    rounds = 3
+
+    # load testing data --------------------------------
+    seed_everything(42)
+    model_shape_dict = {
+        f'aaa_{i}': (*np.random.randint(1, 10, size=np.random.randint(3)),
+            (np.random.randint(10_000, 100_000)*1000)//1000)
+        for i in range(10)
+    }
+
+    grad_test_data = [
+            [{k: torch.randn(v).to('cuda') * 2 - 1 for k, v in model_shape_dict.items()}
+            for _ in range(worker_count)]
+        for _ in range(rounds)]
+
+    # simulate the WZ encoding and reconstruction process --------------------------------
+    broadcast_prot = WZBroadcastProtocol(worker_count,'RNN',
+                train_sample_size=100_000, metric_report_flag=True, lr=1e-5, num_planes=3, bins_per_plane=2)
+    prev = []
+    for round, grad_per_round in enumerate(grad_test_data):
+        for ag_id, grad in enumerate(grad_per_round):
+            broadcast_data = broadcast_prot.wz_encoding_process(grad, ag_id)
+            decoded_agent_broadcast = broadcast_prot.wz_reconstruction_process(
+                broadcast_data, ag_id, worker_count, model_shape_dict, prev, )
+            prev.append(decoded_agent_broadcast)
+
+    # check output size and correctness
+    for i, grad in enumerate(grad_test_data[-1]):
+        assert all([k in grad for k in model_shape_dict.keys()])
+        assert all([v.shape == model_shape_dict[k] for k, v in grad.items()])
