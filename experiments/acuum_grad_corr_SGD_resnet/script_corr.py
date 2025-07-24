11d6dc6ea4b8dbda86b31e3f8da41cff332bf75b
HosseinOutward <hossenmsignin@gmail.com>
2025-07-25 03:02:35 +0330

    completed the correlation experiment and added the result file. also fixed a bug regarding steps counting

--- experiments/acuum_grad_corr_SGD_resnet/script_corr.py
+++ experiments/acuum_grad_corr_SGD_resnet/script_corr.py
@@ -15,9 +15,9 @@ res_per_step = None
 batch_count = None
 client_epochs_per_round = None
 
+min_window_size = 10
 def compute_corr(worker_id, round_number, accum_grads,):
     max_window_size = int(batch_count*client_epochs_per_round*1.2)
-    min_window_size = 10
     flatten_vec = torch.concatenate([v.flatten().cpu() for v in accum_grads.values()]).to(torch.float16)
     if worker_id == 0:
         w0_accu_grads_list.append(flatten_vec)
@@ -65,7 +65,7 @@ class CorrResNetPLModel(ResNetPLModel):
 
         # write the accum grad to disk
         accum_grad = {k: v.detach().clone() for k, v in self.accu_param_grads.items()}
-        round_number = curr_round + (current_epoch + batch_idx)/client_epochs_per_round / batch_count
+        round_number = curr_round + (current_epoch + batch_idx / batch_count)/client_epochs_per_round
         compute_corr(worker_id, round_number, accum_grad)
 
     def clone(self, copy=None):
@@ -75,6 +75,8 @@ class CorrResNetPLModel(ResNetPLModel):
 
 
 # %%
+__batch_size__ = 7_500  # 3 batches per w = 73000/2worker/13000  -  np.ceil(len(dataset[0])/batch_size/2)
+__epoch_count__ = 10
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
 
@@ -105,20 +107,18 @@ if __name__ == '__main__':
             ])
         ) for s in ['train', 'test']]
 
-    dataset = [torch.utils.data.Subset(d, list(range(100))) for d in dataset]
-    for i in range(10):
-        for d in dataset:
-            d.dataset.labels[i]=i
+    # dataset = [torch.utils.data.Subset(d, list(range(100))) for d in dataset]
+    # for i in range(10):
+    #     for d in dataset:
+    #         d.dataset.labels[i]=i
 
     # %%
     w0_accu_grads_list = []
     w1_accu_grads_list = []
     res_per_step = []
 
-    client_epochs_per_round = 10
 
-    batch_size = 7_500  # 3 batches per w = 73000/2worker/13000  -  np.ceil(len(dataset[0])/batch_size/2)
-    batch_count = np.ceil(len(dataset[0])/batch_size/2)
+    batch_count = np.ceil(len(dataset[0])/__batch_size__/2)
 
     # %%
     model = CorrResNetPLModel(num_classes=10, resnet_version='resnet18', lr=0.005, )
@@ -126,8 +126,8 @@ if __name__ == '__main__':
 
     # *****************
     sim = FLSimulator(
-        pl_model=model, num_agents=2, communication_rounds=50, client_epochs_per_round=client_epochs_per_round,
-        batch_size=batch_size, dataset_train=dataset[0], dataset_test=dataset[1],
+        pl_model=model, num_agents=2, communication_rounds=50, client_epochs_per_round=__epoch_count__,
+        batch_size=__batch_size__, dataset_train=dataset[0], dataset_test=dataset[1],
         aggregation_method='fedavg', non_iid_sampling=False, user_logger=None)
     # ****
     sim.run_simulation()
