2e6505b4942574f1f43424a17d83028ac504b021
Hossein <hossenmsignin@gmail.com>
2025-09-12 16:29:06 +0200

    modification to how cancer works

--- components/broadcast_components/broadcasting_process/CancerProt.py
+++ components/broadcast_components/broadcasting_process/CancerProt.py
@@ -1,16 +1,13 @@
 import numpy as np
-
-from components.broadcast_components.WZ_models.WZQuantizerWithDataPrep import QuantizerWithDataPrep
+from components.broadcast_components.WZ_models.WZQuantizerWithDataPrep import QuantizerWithDataPrep, _get_vec_slices
 from components.broadcast_components.broadcasting_process.HybridWZBroadcastProtocol import HybridWZBroadcastProtocol
+from components.broadcast_components.broadcasting_process.ServerTrainingPerRoundProtocol import _train_model
 
 
 def _update_wz_quant_model(qz_model, grad_vector, side_info_vs, epochs):
     assert len(side_info_vs) != 0
-    if epochs > 0:
-        temp = np.random.normal(0, np.sqrt(1e-8), len(grad_vector), ).astype(np.float32)
-        qz_model.train_model(grad_vector + temp, side_info_vs, epoch=epochs, batch_size=10_000)
-    else:
-        print('--- only updating the p_cdf ---')
+    temp = np.random.normal(0, np.sqrt(1e-8), len(grad_vector), ).astype(np.float32)
+    qz_model.train_model(grad_vector + temp, side_info_vs, epoch=epochs, batch_size=10_000)
     qz_model.get_set_training_posterior_cdf(grad_vector, side_info_vs)
     return qz_model
 
@@ -19,40 +16,78 @@ class CancerProtocol(HybridWZBroadcastProtocol):
     def __init__(self, agent_count, wz_base_quantizer: QuantizerWithDataPrep,
                  small_update=False, update_interval=10, **kwargs):
         self.small_update = small_update
+        assert self.small_update == False
+
         self.update_interval = update_interval
         assert update_interval > 3
         super().__init__(agent_count, wz_base_quantizer, hybrid_round_num=self.update_interval, **kwargs)
-        self.si_window_size = self.update_interval-1
+        self.si_window_size = self.update_interval+1
+
         self.cancer_warmup_done = False
+
         self.frozen_quantizers = None
+        self.frozen_si = None
+
+        self.is_freezing_time = lambda round_id: (round_id-1) % self.update_interval == 0
+
+    def _get_side_info_for_grad_recons(self, agent_id, **kwargs):
+        if self.is_freezing_time(self.curr_round_id):
+            assert self.frozen_quantizers is not None
+            assert self.is_hybrid_round_f(self.curr_round_id)
+
+            return self.frozen_si[agent_id]
+
+        return super()._get_side_info_for_grad_recons(agent_id, **kwargs)
 
     def _post_reconstruction_processing(self, agent_id, worker_count, dict_shape, curr_recons_vector):
         super()._post_reconstruction_processing(agent_id, worker_count, dict_shape, curr_recons_vector)
 
-        if not self.cancer_warmup_done and (self.curr_round_id==self.update_interval+1 and agent_id+1==worker_count-1):
-            self.cancer_warmup_done=True
+        next_agent = (agent_id + 1) % worker_count
+        coming_round = self.curr_round_id + int(next_agent==0)
+        coming_is_freezing = self.is_freezing_time(coming_round)
+
+        # first time we are switching to frozen quantizers
+        if coming_is_freezing and not self.cancer_warmup_done:
+            assert next_agent==0
+            assert self.is_hybrid_round_f(self.curr_round_id)
+            assert self.frozen_quantizers is None
+
             print('--- switching to frozen quantizers for cancer protocol ---')
-            self.frozen_quantizers = [a for a in self.wz_quantizer_list]
+            self.cancer_warmup_done=True
+            self.frozen_quantizers = []
+            self.frozen_si = []
 
-        # retrain the qz when we have fresh workerside grads (i.e. all-out rounds have passed)
-        if self.cancer_warmup_done:
-            target_vec = self.past_worker_grad_recons_vec[agent_id][-1]
-            side_info = self._get_side_info_for_grad_recons(agent_id, force_is_hybrid_round=False)
+            for i in range(worker_count):
+                target_vec = self.past_worker_grad_recons_vec[i][-1]
+                side_info = self._get_side_info_for_grad_recons(i, force_is_hybrid_round=False)
+                qz_model = _train_model(
+                        target_vec, side_info, self.wz_basic_quantizer, self.epoch_count,
+                        bins_per_plane=int(max(16 // (self.curr_round_id/2 + 1), 4)),
+                        binary_quant=self.binary_quantizer if self.curr_round_id >= 12 else False,
+                        vec_slices=_get_vec_slices(dict_shape),
+                        user_logger=self.wz_basic_quantizer.user_logger
+                    )
 
-            epoch_count = 0
-            if self.is_hybrid_round_f(self.curr_round_id):
-                print('--- updating frozen quantizers after worker-side ---')
+                self.frozen_quantizers.append(qz_model)
+                self.frozen_si.append(side_info)
 
-                # since its hybrid round, our quantizer is worker-side, so switch back to frozen versions
-                self.wz_quantizer_list[agent_id] = self.frozen_quantizers[agent_id]
+        elif coming_is_freezing:
+            assert next_agent==0
+            assert self.frozen_quantizers is not None
+            assert len(self.frozen_quantizers) == worker_count
+            assert len(self.frozen_si) == worker_count
+            assert self.is_hybrid_round_f(self.curr_round_id)
 
-                epoch_count = 30
-            elif self.small_update:
-                print('--- small updating of frozen quantizers ---')
-                epoch_count = 3
+            print('--- updating frozen quantizers after worker-side ---')
+            for i in range(worker_count):
+                target_vec = self.past_worker_grad_recons_vec[i][-1]
+                side_info = self._get_side_info_for_grad_recons(i, force_is_hybrid_round=False)
+                qz_model = self.wz_quantizer_list[agent_id]
+                qz_model = _update_wz_quant_model(qz_model, target_vec, side_info, 30)
 
-            self.wz_quantizer_list[agent_id] = _update_wz_quant_model(
-                self.wz_quantizer_list[agent_id], target_vec, side_info, epoch_count)
+                self.frozen_quantizers[i]=qz_model
+                self.frozen_si[i]=side_info
+
 
 if __name__ == "__main__":
     from components.broadcast_components.broadcasting_process.ServerTrainingPerRoundProtocol import _test_main
